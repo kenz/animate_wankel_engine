18465e63474bb61b356768228d2163f0153eee34
kenji_matsuoka <kenz@kenjimaokanoair.lan>
2021-07-23 13:35:32 +0900

    refactoring

--- wankel.js
+++ wankel.js
@@ -8,18 +8,8 @@ const ANGLE_360 = Math.PI * 2;
 // 数字を減らすほどより飛ばして描くので回転が速くなります
 const RATE = 1000; // (1/1000度ごとに描く)
 
-// 描画用のキャンバス
-let canvas = document.getElementById('canvas');
-let context = canvas.getContext('2d');
-
-// ロータリーのサイズ
-let rotarySize;
-// 偏向する円の半径
-let deflection;
-// 重心からローターの頂点までの距離
-let distanceOfGravity;
-
-let canvasDiv = document.getElementById('canvas_div');
+// 描画用のキャンバスのContext
+let context;
 // スクリーン幅
 let width;
 // スクリーン高さ
@@ -28,18 +18,26 @@ let height;
 let centerX;
 // スクリーン画面高さ中央
 let centerY;
-
+// ロータリーのサイズ
+let rotarySize;
+// 偏向する円の半径
+let lengthDeflection;
+// 重心からローターの頂点までの距離
+let lengthCornerGravity;
 // スクリーンサイズ変更時に座標を求め直す
 function resetScreenSize() {
+    let canvas = document.getElementById('canvas');
+    let canvasDiv = document.getElementById('canvas_div');
     width = canvas.width = canvasDiv.offsetWidth;
     height = canvas.height = canvasDiv.offsetHeight;
+    context = canvas.getContext('2d');
     // 高さと幅の内狭い方をもとに全体のサイズを決める
-    let narrow = Math.min(width, height);
     centerX = width / 2;
     centerY = height / 2;
-    rotarySize = narrow / 2;
-    deflection = (2 / Math.sqrt(3) - 1) * rotarySize / 2;
-    distanceOfGravity = rotarySize / Math.sqrt(3);
+    rotarySize = Math.min(width, height) / 2;
+    // 前回は直径で求めたがプログラム上では半径のほうが扱いやすいので/2する
+    lengthDeflection = (2 / Math.sqrt(3) - 1) * rotarySize / 2;
+    lengthCornerGravity = rotarySize / Math.sqrt(3);
 }
 resetScreenSize();
 
@@ -47,10 +45,12 @@ window.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('resize', resetScreenSize);
 });
 
+window.requestAnimationFrame(drawAnimation);
 function drawAnimation(timestamp) {
     context.clearRect(0, 0, width, height);
-    drawHousing();
+    //   drawHousing();
     drawRotary(timestamp);
+    window.requestAnimationFrame(drawAnimation);
 }
 
 // ハウジングを描画する
@@ -59,55 +59,51 @@ function drawHousing() {
     context.strokeStyle = 'rgb(0, 0, 0)';
     for (rotaryAngle = 0; rotaryAngle < ANGLE_360; rotaryAngle += 0.001) {
         let shaftAngle = (rotaryAngle + ANGLE_180) * 3;
-        let x = (Math.cos(shaftAngle) * deflection) + (Math.cos(rotaryAngle) * distanceOfGravity) + centerX;
-        let y = (Math.sin(shaftAngle) * deflection) + (Math.sin(rotaryAngle) * distanceOfGravity) + centerY;
+        let x = (Math.cos(shaftAngle) * lengthDeflection) + (Math.cos(rotaryAngle) * lengthCornerGravity) + centerX;
+        let y = (Math.sin(shaftAngle) * lengthDeflection) + (Math.sin(rotaryAngle) * lengthCornerGravity) + centerY;
         context.lineTo(x, y);
     }
-    context.moveTo(centerX + deflection, centerY);
-    context.arc(centerX, centerY, deflection, 0, ANGLE_360);
+    context.stroke();
+    context.beginPath();
+    context.arc(centerX, centerY, lengthDeflection, 0, ANGLE_360);
     context.closePath();
     context.stroke();
 }
 
+// ロータリーを描画する。
 function drawRotary(timestamp) {
-    let rotaryAngle = timestamp / RATE % ANGLE_360;
-    let shaftAngle = rotaryAngle * 3;
-    let x = Math.cos(shaftAngle) * deflection + centerX;
-    let y = Math.sin(shaftAngle) * deflection + centerY;
-    let angle1 = rotaryAngle + ANGLE_180;
-    let angle2 = angle1 + ANGLE_120;
-    let angle3 = angle2 + ANGLE_120;
+    let corner1angle = timestamp / RATE % ANGLE_360;
+    let corner2angle = corner1angle + ANGLE_120;
+    let corner3angle = corner2angle + ANGLE_120;
+    let shaftAngle = (corner1angle + ANGLE_180) * 3;
+    let baseX = Math.cos(shaftAngle) * lengthDeflection + centerX;
+    let baseY = Math.sin(shaftAngle) * lengthDeflection + centerY;
 
     context.beginPath();
     context.fillStyle = 'rgb(255, 0, 0)';
-    context.arc(x, y, 2, 0, ANGLE_360);
+    context.arc(baseX, baseY, 2, 0, ANGLE_360);
     context.fill();
 
     context.beginPath();
     context.strokeStyle = 'rgb(0, 0, 0)';
-    context.arc(x, y, deflection * 2, 0, ANGLE_360);
+    context.arc(baseX, baseY, lengthDeflection * 2, 0, ANGLE_360);
     context.stroke();
 
-    drawSide(x, y, angle1, 'rgb(255,0,0)');
-    drawSide(x, y, angle2, 'rgb(0,128,0');
-    drawSide(x, y, angle3, 'rgb(0,128,255)');
-    window.requestAnimationFrame(drawAnimation);
-
+    drawSide(baseX, baseY, corner1angle, 'rgb(255,0,0)');
+    drawSide(baseX, baseY, corner2angle, 'rgb(0,128,0');
+    drawSide(baseX, baseY, corner3angle, 'rgb(0,128,255)');
 
 }
 
 function drawSide(baseX, baseY, angle, color) {
-    context.fillStyle = color;
-    let x = baseX + Math.cos(angle) * distanceOfGravity;
-    let y = baseY + Math.sin(angle) * distanceOfGravity;
+    let cornerX = baseX + Math.cos(angle) * lengthCornerGravity;
+    let cornerY = baseY + Math.sin(angle) * lengthCornerGravity;
     context.beginPath();
-    context.arc(x, y, 4, 0, ANGLE_360);
+    context.fillStyle = color;
+    context.arc(cornerX, cornerY, 4, 0, ANGLE_360);
     context.fill();
     context.beginPath();
     context.strokeStyle = color;
-    context.arc(x, y, rotarySize, angle + ANGLE_150, angle + ANGLE_210);
-
+    context.arc(cornerX, cornerY, rotarySize, angle + ANGLE_150, angle + ANGLE_210);
     context.stroke();
 }
-
-window.requestAnimationFrame(drawAnimation);
